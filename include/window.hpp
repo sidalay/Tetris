6a1e589fb1316b876f2c816474232d2367fa12fa
Sid Shingaki <theshatteredmind@gmail.com>
2023-04-11 03:40:21 -0700

    chore: initial impl

--- include/window.hpp
+++ include/window.hpp
@@ -1,9 +1,17 @@
 #ifndef WINDOW_HPP
 #define WINDOW_HPP
 
+#include "raylib.h"
+
+#include <string>
+
 struct Window
 {
-  
+  int width{1280};
+  int height{720};
+  std::string title{"tetris"};
+
+  void Tick();
 };
 
 #endif // WINDOW_HPP
