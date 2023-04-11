5b079bf66451a0e15d59b85291d3c0b990fc9810
Sid Shingaki <theshatteredmind@gmail.com>
2023-04-11 05:23:45 -0700

    chore: initial matrix impl

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -1,6 +1,49 @@
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
 
+#include "raylib.h"
+#include "tetromino.hpp"
 
+#include <vector>
+
+struct Cell
+{
+  Rectangle area{};
+  Color     color{};
+  bool      occupied{};
+};
+
+struct Frame
+{
+  int x{};
+  int y{};
+};
+
+struct Playfield
+{
+  Frame              border{300, 600};
+  Frame              hold{120, 120};
+  Frame              next{120, 120};
+  Frame              preview{120, 360};
+  int                spacing{30};
+  Vector2            grid{10, 24};
+  std::vector<Cell>  matrix{};
+
+  Playfield();
+  void Tick();
+  void Draw();
+  void DrawBorder();
+  void DrawMatrix();
+  void DrawHold();
+  void DrawNext();
+  void DrawPreview();
+  void InitializeMatrix();
+};
+
+// assume for 1280x720 screen 1 Tetromino Square is 30px x 30px 
+// grid is 10 wide x 24 high
+// 720 / 24 = 30. 30 x 10 = 300. 1280 - 300 = 980. 980 / 2 = 490.
+// hold, next should be 5 tetromino square x 5 tetromino square
+// preview should be 5 tetromino square x 15 tetromino square (preview 3 pieces)
 
 #endif // MATRIX_HPP
--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,1 +1,63 @@
 #include "matrix.hpp"
+
+Playfield::Playfield()
+{
+  InitializeMatrix();
+}
+
+void Playfield::Tick()
+{
+
+}
+
+void Playfield::Draw()
+{
+  DrawBorder();
+  DrawMatrix();
+}
+
+void Playfield::DrawBorder()
+{
+  DrawRectangleLines(489, 59, border.x+2, border.y+2, WHITE);
+  DrawRectangleLines(489 - (hold.x + spacing), 59, hold.x+2, hold.y+2, WHITE);
+  DrawRectangleLines(489 + border.x + spacing, 59, next.x+2, next.y+2, WHITE);
+  DrawRectangleLines(489 + border.x + spacing, 59 + preview.x + spacing, preview.x+2, preview.y+2, WHITE);
+}
+
+void Playfield::DrawMatrix()
+{
+  for (auto& cell : matrix) {
+    DrawRectangle(cell.area.x, cell.area.y, cell.area.width, cell.area.height, cell.color);
+    DrawRectangleLines(cell.area.x, cell.area.y, cell.area.width, cell.area.height, BLACK);
+  }
+}
+
+void Playfield::DrawHold()
+{
+  
+}
+
+void Playfield::DrawNext()
+{
+
+}
+
+void Playfield::DrawPreview()
+{
+  
+}
+
+void Playfield::InitializeMatrix()
+{
+  int count{};
+  matrix.reserve(240);
+  for (float y{}; y < border.y; y += border.y / grid.y) {
+    for (float x{}; x < border.x; x += border.x / grid.x) {
+      if (static_cast<int>(y) % 2 == 0) {
+        matrix.emplace_back(Rectangle{490 + x, 60 + y, border.x / grid.x, border.y / grid.y}, count++ % 2 == 0 ? Color{ 15, 14, 14, 255 } : Color{ 20, 19, 19, 255 });
+      } else {
+        matrix.emplace_back(Rectangle{490 + x, 60 + y, border.x / grid.x, border.y / grid.y}, count++ % 2 != 0 ? Color{ 15, 14, 14, 255 } : Color{ 20, 19, 19, 255 });
+      }
+    }
+  }
+}
