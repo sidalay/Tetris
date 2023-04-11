8e38db199a8334cdb0452d8a793fb2f338eacde6
Sid Shingaki <theshatteredmind@gmail.com>
2023-04-10 21:58:37 -0700

    feat: struct randomizer

---
+++ include/randomizer.hpp
@@ -0,0 +1,16 @@
+#ifndef RANDOMIZER_HPP
+#define RANDOMIZER_HPP
+
+#include <random>
+
+struct Randomizer
+{
+public:
+  Randomizer() = default;
+  int Randomize(std::uniform_int_distribution<int>& Range);
+private:
+  std::random_device  Seed;
+  std::mt19937        RandomEngine{std::mt19937{Seed()}};
+};
+
+#endif // RANDOMIZER_HPP
