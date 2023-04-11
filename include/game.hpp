5df10a96910c52ef9021f05e06b85b7f455ec382
Sid Shingaki <theshatteredmind@gmail.com>
2023-04-11 03:40:58 -0700

    chore: Initialize() & Tick() declared

--- include/game.hpp
+++ include/game.hpp
@@ -1,17 +1,27 @@
 #ifndef GAME_HPP
 #define GAME_HPP
 
+#include "window.hpp"
+
 namespace Game
 {
+  enum class State
+  {
+
+  };
+
   struct Properties
   {
     float deltatime{};
+    float updatetime{1.f/144.f};
     float lockdown{};
+    int   fps{144};
+    Window window{};
   };
 
   void Run();
-  void Tick();
-  void Initialize();
+  void Initialize(Properties&);
+  void Tick(Properties&);
 }
 
 #endif // GAME_HPP
