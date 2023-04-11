41c9ad915d2db776fa154cdaca4eaa194142b25f
Sid Shingaki <theshatteredmind@gmail.com>
2023-04-11 03:40:27 -0700

    chore: tick() impl

--- src/window.cpp
+++ src/window.cpp
@@ -1,1 +1,12 @@
 #include "window.hpp"
+
+void Window::Tick()
+{
+  if (IsKeyDown(KEY_LEFT_ALT) && IsKeyPressed(KEY_ENTER)) {
+    ToggleFullscreen();
+  }
+
+  if (width != GetScreenWidth()) {
+    SetWindowSize(width, height);
+  }
+}
