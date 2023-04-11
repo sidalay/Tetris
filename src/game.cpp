e6375f36599a86598849a0d1ddc0fab1ccf0857d
Sid Shingaki <theshatteredmind@gmail.com>
2023-04-11 03:41:28 -0700

    feat: Run(), Initialize(), Tick() impl

--- src/game.cpp
+++ src/game.cpp
@@ -2,5 +2,35 @@
 
 void Game::Run()
 {
-  
+  Properties game{};
+  Game::Initialize(game);
+
+  if (IsWindowReady()) {
+    while (!WindowShouldClose()) {
+      Game::Tick(game);
+    }
+  }
+
+  CloseWindow();
+}
+
+void Game::Initialize(Properties& game)
+{
+  SetTraceLogLevel(LOG_WARNING);
+  SetTargetFPS(game.fps);
+  InitWindow(game.window.width, game.window.height, game.window.title.c_str());
+}
+
+void Game::Tick(Properties& game)
+{
+  game.deltatime += GetFrameTime();
+
+  if (game.deltatime >= game.updatetime) {
+    game.window.Tick();
+    game.deltatime = 0.f;
+  }
+
+  BeginDrawing();
+  ClearBackground(BLACK);
+  EndDrawing();
 }
