1757087876b5f8a13946aeef60eda24f4096818c
Sid Shingaki <theshatteredmind@gmail.com>
2023-04-12 05:10:34 -0700

    feat: matrix grid now scales with window size
    - UpdateMatrix() impl
    - Init Border frame in ctor
    - DrawMatrix() impl
    - if check in UpdateFrames()
    - remove old commented code

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -5,7 +5,7 @@
 #include "tetromino.hpp"
 #include "window.hpp"
 
-#include <vector>
+#include <array>
 
 struct Cell
 {
@@ -22,12 +22,9 @@ struct Frame
 
 struct Playfield
 {
-  Frame              border{};
-  Frame              hold{120, 120};
-  Frame              preview{120, 240};
-  int                spacing{30};
-  Vector2            grid{10, 24};
-  std::vector<Cell>  matrix{};
+  Frame border{};
+  Frame hold{};
+  std::array<std::array<Cell,10>,24> matrix{};
 
   Playfield();
   void Tick();
@@ -35,16 +32,11 @@ struct Playfield
   void DrawBorder();
   void DrawMatrix();
   void DrawHold();
+  void DrawNext();
   void DrawPreview();
   void InitializeMatrix();
   void UpdateFrames();
   void UpdateMatrix();
 };
-
-// assume for 1280x720 screen 1 Tetromino Square is 30px x 30px 
-// grid is 10 wide x 24 high
-// 720 / 24 = 30. 30 x 10 = 300. 1280 - 300 = 980. 980 / 2 = 490.
-// hold, next should be 5 tetromino square x 5 tetromino square
-// preview should be 5 tetromino square x 15 tetromino square (preview 3 pieces)
 
 #endif // MATRIX_HPP
--- src/matrix.cpp
+++ src/matrix.cpp
@@ -4,11 +4,17 @@ const float cell_size{.045f}; // 4.5% of height
 const float well_width{10.f * cell_size}; // 10 cells wide
 const float w_margin{(1.f - well_width) * .5f}; // 22.5% margin
 const float h_margin{.05f}; // 5% from top/bottom of screen
-const float well_height{1 - (h_margin * 2.f)}; // 90% of height  
+const float well_height{1 - (h_margin * 2.f)}; // 90% of height
+const float frame_spacing{cell_size};  
 
 Playfield::Playfield()
+  : border{
+      Window::width * ((1.f - ((Window::height * well_width)/Window::width)) * .5f),
+      Window::height * h_margin,
+      Window::height * well_width,
+      Window::height * well_height
+    }
 {
-  UpdateFrames();
   InitializeMatrix();
 }
 
@@ -20,29 +26,38 @@ void Playfield::Tick()
 
 void Playfield::Draw()
 {
-  // DrawMatrix();
+  DrawMatrix();
   DrawBorder();
 }
 
 void Playfield::DrawBorder()
 {
-  DrawRectangleLines(border.area.x, border.area.y, border.area.width, border.area.height, WHITE);
-  // DrawRectangleLines(489 - (hold.area.x + spacing), 59, hold.area.x+2, hold.area.y+2, WHITE);
-  // DrawRectangleLines(489 + border.area.x + spacing, 59, hold.area.x+2, hold.area.y+2, WHITE);
-  // DrawRectangleLines(489 + border.area.x + spacing, 59 + preview.area.x + spacing, preview.area.x+2, preview.area.y+2, WHITE);
+  DrawRectangleLinesEx(border.area, 1.f, Color{ 78, 78, 78, 100 });
 }
 
 void Playfield::DrawMatrix()
 {
-  for (auto& cell : matrix) {
-    DrawRectangle(cell.area.x, cell.area.y, cell.area.width, cell.area.height, cell.color);
-    DrawRectangleLines(cell.area.x, cell.area.y, cell.area.width, cell.area.height, BLACK);
+  for (auto& row : matrix) {
+    for (auto& col : row) {
+      DrawRectangle(
+        col.area.x, 
+        col.area.y, 
+        col.area.width, 
+        col.area.height, 
+        col.color);
+      DrawRectangleLinesEx(col.area, 2.f, Color{ 48, 48, 48, 100 });
+    }
   }
 }
 
 void Playfield::DrawHold()
 {
-  
+  DrawRectangleLinesEx(hold.area, 1.f, Color{ 78, 78, 78, 100 });
+}
+
+void Playfield::DrawNext()
+{
+
 }
 
 void Playfield::DrawPreview()
@@ -50,47 +65,54 @@ void Playfield::DrawPreview()
   
 }
 
-// void Playfield::InitializeMatrix()
-// {
-//   int count{};
-//   matrix.reserve(240);
-//   for (float y{0}; y < border.area.height + (4 * (Window::height * cell_size)); y += Window::height * cell_size) {
-//     for (float x{1}; x < border.area.width; x += Window::height * cell_size) {
-//       if (static_cast<int>(y) % 2 == 0) {
-//         matrix.emplace_back(
-//           Rectangle{border.area.x + x, 
-//                     border.area.y + y, 
-//                     Window::height * cell_size, 
-//                     Window::height * cell_size}, 
-//                     count++ % 2 == 0 ? Color{ 15, 14, 14, 255 } : Color{ 20, 19, 19, 255 });
-//       } else {
-//         matrix.emplace_back(
-//           Rectangle{border.area.x + x, 
-//                     border.area.y + y, 
-//                     Window::height * cell_size, 
-//                     Window::height * cell_size}, 
-//                     count++ % 2 != 0 ? Color{ 15, 14, 14, 255 } : Color{ 20, 19, 19, 255 });
-//       }
-//     }
-//   }
-// }
-
 void Playfield::InitializeMatrix()
 {
-  
+  for (int y{}; y < matrix.size() - 4; ++y) {
+    for (int x{0}; x < matrix[y].size(); ++x) {
+      if (y % 2 == 0) {
+        matrix[y][x].area.x = border.area.x + ((Window::height * cell_size) * x);
+        matrix[y][x].area.y = border.area.y + ((Window::height * cell_size) * y);
+        matrix[y][x].area.width = Window::height * cell_size;
+        matrix[y][x].area.height = Window::height * cell_size;
+        matrix[y][x].color = x % 2 == 0 ? Color{ 15, 14, 14, 255 } : Color{ 20, 19, 19, 255 };
+      } else {
+        matrix[y][x].area.x = border.area.x + ((Window::height * cell_size) * x);
+        matrix[y][x].area.y = border.area.y + ((Window::height * cell_size) * y);
+        matrix[y][x].area.width = Window::height * cell_size;
+        matrix[y][x].area.height = Window::height * cell_size;
+        matrix[y][x].color = x % 2 != 0 ? Color{ 15, 14, 14, 255 } : Color{ 20, 19, 19, 255 };
+      }
+    }
+  }
 }
 
 void Playfield::UpdateFrames()
 {
-  border.area.x = Window::width * ((1.f - ((Window::height * well_width)/Window::width)) * .5f);
-  border.area.y = Window::height * h_margin;
-  border.area.width = Window::height * well_width;
-  border.area.height = Window::height * well_height;
+  if (IsWindowResized()) {
+    border.area.x = Window::width * ((1.f - ((Window::height * well_width)/Window::width)) * .5f);
+    border.area.y = Window::height * h_margin;
+    border.area.width = Window::height * well_width;
+    border.area.height = Window::height * well_height;
+  }
 }
 
 void Playfield::UpdateMatrix()
 {
   if (IsWindowResized()) {
-
+    for (int y{}; y < matrix.size() - 4; ++y) {
+      for (int x{0}; x < matrix[y].size(); ++x) {
+        if (y % 2 == 0) {
+          matrix[y][x].area.x = border.area.x + ((Window::height * cell_size) * x);
+          matrix[y][x].area.y = border.area.y + ((Window::height * cell_size) * y);
+          matrix[y][x].area.width = Window::height * cell_size;
+          matrix[y][x].area.height = Window::height * cell_size;
+        } else {
+          matrix[y][x].area.x = border.area.x + ((Window::height * cell_size) * x);
+          matrix[y][x].area.y = border.area.y + ((Window::height * cell_size) * y);
+          matrix[y][x].area.width = Window::height * cell_size;
+          matrix[y][x].area.height = Window::height * cell_size;
+        }
+      }
+    }
   }
 }
