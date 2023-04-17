298a72f8a5cfd7505c45e88bbcde975a8a10f603
Sid Shingaki <theshatteredmind@gmail.com>
2023-04-16 19:40:35 -0700

    feat: CheckOutOfBounds() impl

--- include/enforcer.hpp
+++ include/enforcer.hpp
@@ -1,9 +1,12 @@
 #ifndef ENFORCER_HPP
 #define ENFORCER_HPP
 
-struct Enforcer
-{
+#include "matrix.hpp"
+#include "tetromino.hpp"
 
+namespace Enforcer
+{
+  bool CheckOutOfBounds(const Tetromino&, const Playfield&, const Tetro::Orientation);
 };
 
 #endif // ENFORCER_HPP
--- src/enforcer.cpp
+++ src/enforcer.cpp
@@ -1,1 +1,30 @@
 #include "enforcer.hpp"
+
+#include "window.hpp"
+
+bool Enforcer::CheckOutOfBounds(const Tetromino& t, const Playfield& p, const Tetro::Orientation direction)
+{
+  auto cell_size{Window::height * Window::cell_size};
+  auto blocks{t.GetBlocks()};
+  auto border{p.GetBorder()};
+
+  switch (direction)
+  {
+    case Tetro::Orientation::LEFT:
+      for (auto& block : blocks) {
+        if (block.area.x < border.x) {
+          return true;
+        } 
+      }
+      break;
+    case Tetro::Orientation::RIGHT:
+      for (auto& block : blocks) {
+        if (block.area.x + cell_size >= border.x + border.width) {
+          return true;
+        }
+      }
+      break;
+  }
+
+  return false;
+}
