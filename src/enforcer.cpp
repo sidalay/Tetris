d15b10e2a62c8c63697e930e6fef8dc033849504
Sid Shingaki <theshatteredmind@gmail.com>
2023-04-16 21:05:45 -0700

    refactor: switch() -> if()

--- src/enforcer.cpp
+++ src/enforcer.cpp
@@ -2,29 +2,29 @@
 
 #include "window.hpp"
 
+bool Enforcer::CheckOccupancy()
+{
+  return false;
+}
+
 bool Enforcer::CheckOutOfBounds(const Tetromino& t, const Playfield& p, const Tetro::Orientation direction)
 {
   float cell_size{Window::height * Window::cell_size};
   auto blocks{t.GetBlocks()};
   auto border{p.GetBorder()};
 
-  switch (direction)
-  {
-    case Tetro::Orientation::LEFT:
-      for (auto& block : blocks) {
-        if (block.area.x < border.x) {
-          return true;
-        } 
+  if (direction == Tetro::Orientation::LEFT) {
+    for (auto& block : blocks) {
+      if (block.area.x < border.x) {
+        return true;
+      } 
+    }
+  } else if (direction == Tetro::Orientation::RIGHT) {
+    for (auto& block : blocks) {
+      if (block.area.x + cell_size >= border.x + border.width) {
+        return true;
       }
-      break;
-    case Tetro::Orientation::RIGHT:
-      for (auto& block : blocks) {
-        if (block.area.x + cell_size >= border.x + border.width) {
-          return true;
-        }
-      }
-      break;
+    }
   }
-
   return false;
 }
