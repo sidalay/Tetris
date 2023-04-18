612c9e7f6c6e1b4f5fca68763d05f94d2e7cca32
Sid Shingaki <sidalaytv@gmail.com>
2023-04-17 19:49:46 -0700

    feat: impl IsBelowSafe() & IsSideSafe()

--- src/enforcer.cpp
+++ src/enforcer.cpp
@@ -2,14 +2,14 @@
 
 #include "window.hpp"
 
-bool Enforcer::IsBelowClear(const Tetromino& t, const Playfield& p)
+bool Enforcer::IsBelowSafe(const Tetromino t, const Playfield& p)
 {
   float cell_size{Window::height * Window::cell_size_percentage};
   auto blocks{t.GetBlocks()};
   auto map{p.GetMatrixMap()};
 
   for (auto& block : blocks) {
-    std::pair key{block.area.x, block.area.y + cell_size};
+    std::pair key{static_cast<int>(block.area.x), static_cast<int>(block.area.y + cell_size)};
     bool occupied{map.at(key)};
     if (occupied) {
       return false;
@@ -18,24 +18,29 @@ bool Enforcer::IsBelowClear(const Tetromino& t, const Playfield& p)
   return true;
 }
 
-bool Enforcer::IsOutOfBounds(const Tetromino& t, const Playfield& p, const Tetro::Orientation direction)
+bool Enforcer::IsSideSafe(const Tetromino t, const Playfield& p, const Tetro::Orientation direction)
 {
   float cell_size{Window::height * Window::cell_size_percentage};
   auto blocks{t.GetBlocks()};
-  auto border{p.GetBorder()};
+  auto map{p.GetMatrixMap()};
 
   if (direction == Tetro::Orientation::LEFT) {
     for (auto& block : blocks) {
-      if (block.area.x < border.x) {
-        return true;
-      } 
+      std::pair key{static_cast<int>(block.area.x - cell_size), static_cast<int>(block.area.y)};
+      bool occupied{map.at(key)};
+      if (occupied) {
+        return false;
+      }
     }
   } else if (direction == Tetro::Orientation::RIGHT) {
     for (auto& block : blocks) {
-      if (block.area.x + cell_size >= border.x + border.width) {
-        return true;
+      std::pair key{static_cast<int>(block.area.x + cell_size), static_cast<int>(block.area.y)};
+      bool occupied{map.at(key)};
+      if (occupied) {
+        return false;
       }
     }
   }
-  return false;
+
+  return true;
 }
