d1157a474584a1fa9eba6bc57429934b1c750288
Sid Shingaki <sidalaytv@gmail.com>
2023-04-22 16:00:00 -0700

    chore: add comments

--- src/window.cpp
+++ src/window.cpp
@@ -7,10 +7,10 @@ namespace Window
   int width{1280};
   int height{720};
   std::string title{"tetris"};
-  const float cell_size_percentage{1.f/26.f};            // 100% / 26 cells 
-  const float well_width{10.f * cell_size_percentage};   // 10 cells wide
-  const float well_height{20.f * cell_size_percentage};  // 20 cells high
-  const float h_margin{cell_size_percentage * 3.f};      // height margin = 3 cells
+  const float cell_size_percentage{1.f/26.f};            // 100% / # of cells 
+  const float well_width{10.f * cell_size_percentage};   // # of cells wide
+  const float well_height{20.f * cell_size_percentage};  // # of cells high
+  const float h_margin{cell_size_percentage * 3.f};      // height margin = # of cells
 }
 
 void Window::Tick()
