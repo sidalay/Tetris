66b38349d4480779d73837004d6e36c6156e3671
Sid Shingaki <theshatteredmind@gmail.com>
2023-04-23 16:53:08 -0700

    feat: (WIP) bag struct impl

--- include/bag.hpp
+++ include/bag.hpp
@@ -1,6 +1,28 @@
 #ifndef BAG_HPP
 #define BAG_HPP
 
+#include "randomizer.hpp"
+#include "tetromino.hpp"
 
+#include <vector>
+
+struct Bag
+{
+public:
+  Bag();
+
+  // Retrieve a piece
+  void Pull();
+  // View current Bag
+
+  // Fill the bag
+  void Fill();
+private:
+  std::vector<Tetromino> bag{};
+  Randomizer             randomizer{};
+  
+  // Check capacity
+  bool IsEmpty() const {return bag.empty();}
+};
 
 #endif // BAG_HPP
--- src/bag.cpp
+++ src/bag.cpp
@@ -1,1 +1,22 @@
 #include "bag.hpp"
+
+#include <iostream>
+
+Bag::Bag() 
+{
+  Fill();
+}
+
+void Bag::Fill()
+{
+  std::uniform_int_distribution<int> range{0,6};
+  for (int i{}; i < 7; ++i) {
+    std::cout << randomizer(range) << ' ';
+  }
+}
+
+void Bag::Pull()
+{
+
+}
+
