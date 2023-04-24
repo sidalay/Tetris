ed067e4b265c0ea9680ca68f0a5ab025d2d6496f
Sid Shingaki <sidalaytv@gmail.com>
2023-04-24 00:58:56 -0700

    chore: remove Randomize()
    - add GetEngine()

--- include/randomizer.hpp
+++ include/randomizer.hpp
@@ -8,7 +8,7 @@ struct Randomizer
 public:
   Randomizer() = default;
   int operator()(std::uniform_int_distribution<int>& Range);
-  int Randomize(std::uniform_int_distribution<int>& Range);
+  std::mt19937& GetEngine() {return RandomEngine;}
 private:
   std::random_device  Seed;
   std::mt19937        RandomEngine{std::mt19937{Seed()}};
--- src/randomizer.cpp
+++ src/randomizer.cpp
@@ -4,8 +4,3 @@ int Randomizer::operator()(std::uniform_int_distribution<int>& Range)
 {
   return Range(RandomEngine);
 }
-
-int Randomizer::Randomize(std::uniform_int_distribution<int>& Range)
-{
-  return Range(RandomEngine);
-}
