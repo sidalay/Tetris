33f62b41761f15889f6031dbf766efad16d8791b
Sid Shingaki <sidalaytv@gmail.com>
2023-04-24 19:21:11 -0700

    chore: refactor Randomizer to be more readable

--- src/randomizer.cpp
+++ src/randomizer.cpp
@@ -7,10 +7,10 @@ std::vector<int> Randomizer::operator()()
   std::vector<int> rolled{};
   std::array<double, 7> probabilities{};
   for (double total{7.0}; total > 0.0; --total) {
-    for (auto& chance : probabilities) {
-      chance = 1.0/total;
-    }
-    for (auto& roll : rolled) {
+    // 1.0 / total # of pieces remaining
+    probabilities.fill(1.0/total);
+    // pieces that have been removed should have 0 probability
+    for (auto& roll : rolled) {    
       probabilities.at(roll) = 0.0;
     }
     std::discrete_distribution<int> dist(std::begin(probabilities), std::end(probabilities));
