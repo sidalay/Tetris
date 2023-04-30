3a3350f3f1b1d1d2c58b598f434182eeff66905c
Sid Shingaki <sidalaytv@gmail.com>
2023-04-29 22:05:42 -0700

    chore: update comments

--- include/score.hpp
+++ include/score.hpp
@@ -5,18 +5,18 @@
 
 /*
 
-Single/Mini T-Spin		                        100×level
-Mini T-Spin Single		                        200×level
-Double		                                    300×level
-T-Spin/Mini T-Spin Double		                  400×level
-Triple		                                    500×level
-B2B Mini T-Spin Double		                    600×level
-Tetris/T-Spin Single		                      800×level
-B2B T-Spin Single/B2B Tetris/T-Spin Double		1,200×level
-T-Spin Triple		                              1,600×level
-B2B T-Spin Double		                          1,800×level
-B2B T-Spin Triple		                          2,400×level
-Combo		                                      (move value+50)×level
+Single/Mini T-Spin		                        100    ×  level
+Mini T-Spin Single		                        200    ×  level
+Double		                                    300    ×  level
+T-Spin/Mini T-Spin Double		                  400    ×  level
+Triple		                                    500    ×  level
+B2B Mini T-Spin Double		                    600    ×  level
+Tetris/T-Spin Single		                      800    ×  level
+B2B T-Spin Single/B2B Tetris/T-Spin Double		1,200  ×  level
+T-Spin Triple		                              1,600  ×  level
+B2B T-Spin Double		                          1,800  ×  level
+B2B T-Spin Triple		                          2,400  ×  level
+Combo		                                      (move value+50) × level
 Soft drop		                                  1 point per cell
 Hard drop		                                  2 points per cell
 
