1297569d50018323de58725e5fa2d46a37a87f99
Sid Shingaki <sidalaytv@gmail.com>
2023-05-01 18:50:47 -0700

    chore: menu setup

--- include/menu.hpp
+++ include/menu.hpp
@@ -1,11 +1,49 @@
 #ifndef MENU_HPP
 #define MENU_HPP
 
+#include "play.hpp"
+#include "controller.hpp"
+
 namespace Game
 {
 
-enum class Menu {
-  TITLE, MAIN, MODESELECT, PAUSE, SETTINGS, GAMEOVER, HELP
+class Menu 
+{
+enum class Screen {
+  TITLE, MAIN, PLAY, PAUSE, SETTINGS, GAMEOVER, HELP
+};
+
+public:
+  explicit Menu() = default;
+
+  void Tick();
+  void Draw();
+  [[nodiscard]] constexpr Screen CurrentScreen()const {return screen;}
+
+private:
+  Screen screen{};
+  // Controller controller{};
+
+  void TickTitle();
+  void DrawTitle();
+
+  void TickMain();
+  void DrawMain();
+
+  void TickPlay();
+  void DrawPlay();
+
+  void TickPause();
+  void DrawPause();
+
+  void TickSettings();
+  void DrawSettings();
+
+  void TickGameover();
+  void DrawGameover();
+
+  void TickHelp();
+  void DrawHelp();
 };
 
 }
--- src/menu.cpp
+++ src/menu.cpp
@@ -1,1 +1,113 @@
 #include "menu.hpp"
+
+void Game::Menu::Tick()
+{
+  switch (screen)
+  {
+  case Screen::TITLE:
+    TickTitle();
+    break;
+  case Screen::MAIN:
+    TickMain();
+    break;
+  case Screen::PLAY:
+    TickPlay();
+    break;
+  case Screen::PAUSE:
+    TickPause();
+    break;
+  case Screen::SETTINGS:
+    TickSettings();
+    break;
+  case Screen::GAMEOVER:
+    TickGameover();
+    break;
+  case Screen::HELP:
+    TickHelp();
+    break;
+  }
+}
+
+void Game::Menu::Draw()
+{
+  switch (screen)
+  {
+  case Screen::TITLE:
+    DrawTitle();
+    break;
+  case Screen::MAIN:
+    DrawMain();
+    break;
+  case Screen::PLAY:
+    DrawPlay();
+    break;
+  case Screen::PAUSE:
+    DrawPause();
+    break;
+  case Screen::SETTINGS:
+    DrawSettings();
+    break;
+  case Screen::GAMEOVER:
+    DrawGameover();
+    break;
+  case Screen::HELP:
+    DrawHelp();
+    break;
+  }
+}
+
+void Game::Menu::TickTitle()
+{
+}
+
+void Game::Menu::DrawTitle()
+{
+}
+
+void Game::Menu::TickMain()
+{
+}
+
+void Game::Menu::DrawMain()
+{
+}
+
+void Game::Menu::TickPlay()
+{
+}
+
+void Game::Menu::DrawPlay()
+{
+}
+
+void Game::Menu::TickPause()
+{
+}
+
+void Game::Menu::DrawPause()
+{
+}
+
+void Game::Menu::TickSettings()
+{
+}
+
+void Game::Menu::DrawSettings()
+{
+}
+
+void Game::Menu::TickGameover()
+{
+}
+
+void Game::Menu::DrawGameover()
+{
+}
+
+void Game::Menu::TickHelp()
+{
+}
+
+void Game::Menu::DrawHelp()
+{
+}
